5909a9b305994b320830592b346004617f299201
oycm <1164864987@qq.com>
2024-12-27 20:29:35 +0800

    事件监听发布机制

--- src/main/java/com/oycm/controller/CustomerController.java
+++ src/main/java/com/oycm/controller/CustomerController.java
@@ -1,17 +1,24 @@
 package com.oycm.controller;
 
 import com.oycm.entity.Customer;
+import com.oycm.event.EmailService;
 import com.oycm.service.CustomerService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
 public class CustomerController {
 
+    @Autowired
+    private EmailService emailService;
     private CustomerService customerService;
+    private static Log log = LogFactory.getLog(CustomerController.class);
 
     @Autowired
     public CustomerController(CustomerService customerService) {
@@ -19,7 +26,9 @@ public class CustomerController {
     }
 
     @RequestMapping("/getCus")
-    public List<Customer> getCustomer() {
+    public List<Customer> getCustomer(HttpServletRequest request) {
+        log.info(request.getRequestURI());
+        emailService.sendEmail(request.getRequestURI(), "sendEmail");
         return customerService.getList();
     }
 }
