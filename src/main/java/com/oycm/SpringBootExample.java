855a61cf8d51e6722988849864e2bffd6704d600
oycm <1164864987@qq.com>
2025-04-17 22:02:23 +0800

    kafka初始化例子

--- src/main/java/com/oycm/SpringBootExample.java
+++ src/main/java/com/oycm/SpringBootExample.java
@@ -1,21 +1,30 @@
 package com.oycm;
 
 
+import com.oycm.dao.CustomerRepository;
+import com.oycm.entity.Customer;
+import com.oycm.utils.JsonUtils;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 @RestController
-@SpringBootApplication(exclude = KafkaAutoConfiguration.class)
+@SpringBootApplication()
 @ConfigurationPropertiesScan("com.oycm.config")
 @EntityScan("com.oycm.entity")
 public class SpringBootExample {
@@ -25,6 +34,45 @@ public class SpringBootExample {
     @Value("${name}")
     private String name;
 
+    @Autowired
+    private KafkaTemplate<String, String> template;
+
+    private final CountDownLatch latch = new CountDownLatch(3);
+
+    @Bean
+    CommandLineRunner myCommandLineRunner(CustomerRepository repository) {
+
+        return (String... args) -> {
+            // 新增 4
+            repository.save(new Customer("ouyangcm", "欧阳诚明"));
+            repository.save(new Customer("tangdh", "唐狄豪"));
+            repository.save(new Customer("zhangzx", "张臻祥"));
+            repository.save(new Customer("fangyc", "方越程"));
+
+            // 查询所有
+            log.info("Customers found with findAll():");
+            repository.findAll().forEach(customer -> {
+                log.info(JsonUtils.objToString(customer));
+            });
+
+            // 向kafka发送消息
+            template.send("org.test1", "foo1");
+            template.send("org.test1", "foo2");
+            template.send("org.test1", "foo3");
+
+            // 等待直到 计数器为0
+            latch.await(60, TimeUnit.SECONDS);
+            log.info("All received");
+
+        };
+    }
+
+    // 消费者
+    @KafkaListener(topics = "org.test1")
+    public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+        log.info("consumer: " + cr.toString());
+        latch.countDown();
+    }
 
     @RequestMapping("/")
     String home() {
@@ -34,10 +82,7 @@ public class SpringBootExample {
     }
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootExample.class, args);
-
-        ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
-
+        SpringApplication.run(SpringBootExample.class, args);
     }
 
 
--- src/main/java/com/oycm/service/CustomerService.java
+++ src/main/java/com/oycm/service/CustomerService.java
@@ -5,8 +5,6 @@ import com.oycm.entity.Customer;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -31,39 +29,4 @@ public class CustomerService {
         return list;
     }
 
-    @Bean
-    CommandLineRunner myCommandLineRunner(CustomerRepository repository) {
-
-        return (String... args) -> {
-            // save a few customers
-            repository.save(new Customer("Jack", "Bauer"));
-            repository.save(new Customer("Chloe", "O'Brian"));
-            repository.save(new Customer("Kim", "Bauer"));
-            repository.save(new Customer("David", "Palmer"));
-            repository.save(new Customer("Michelle", "Dessler"));
-
-            // fetch all customers
-            log.info("Customers found with findAll():");
-            log.info("-------------------------------");
-            repository.findAll().forEach(customer -> {
-                log.info(customer.toString());
-            });
-            log.info("");
-
-            // fetch an individual customer by ID
-            Customer customer = repository.findById(1L);
-            log.info("Customer found with findById(1L):");
-            log.info("--------------------------------");
-            log.info(customer.toString());
-            log.info("");
-
-            // fetch customers by last name
-            log.info("Customer found with findByLastName('Bauer'):");
-            log.info("--------------------------------------------");
-            repository.findByLastName("Bauer").forEach(bauer -> {
-                log.info(bauer.toString());
-            });
-            log.info("");
-        };
-    }
 }
