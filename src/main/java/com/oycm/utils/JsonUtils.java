ac50656728dabd6f606aad0e422cdeee3a6466d3
oycm <1164864987@qq.com>
2025-04-19 13:29:34 +0800

    JsonUtils 调整

--- src/main/java/com/oycm/utils/JsonUtils.java
+++ src/main/java/com/oycm/utils/JsonUtils.java
@@ -2,6 +2,7 @@ package com.oycm.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -10,8 +11,13 @@ public class JsonUtils {
     private static final Log log = LogFactory.getLog(JsonUtils.class);
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    static {
+        // 忽略未知属性导致转json失败异常
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+    }
 
     public static String objToString(Object obj) {
+
         try {
             return objectMapper.writerFor(obj.getClass()).writeValueAsString(obj);
         } catch (JsonProcessingException e) {
