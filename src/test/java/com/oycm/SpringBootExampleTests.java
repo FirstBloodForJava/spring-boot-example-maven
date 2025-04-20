26a07d42fe3f97b4124b2af413acfca64b5b1b99
oycm <1164864987@qq.com>
2025-04-20 08:24:38 +0800

    去掉Test

--- src/test/java/com/oycm/SpringBootExampleTests.java
+++ src/test/java/com/oycm/SpringBootExampleTests.java
@@ -1,15 +1,10 @@
 package com.oycm;
 
-import com.oycm.utils.JsonUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @SpringBootTest
@@ -25,13 +20,5 @@ class SpringBootExampleTests {
 
     }
 
-    @Test
-    void exampleTest(@Autowired AcmeProperties acmeProperties) throws Exception {
-        mvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.remoteAddress").exists())
-                .andExpect(content().json(JsonUtils.objToString(acmeProperties)));
-    }
 
 }
